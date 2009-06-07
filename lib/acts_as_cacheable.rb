7e4c519118601a7e237529ac20d881736e3d4ae9
Chris <chrislee1@gmx.de>
2009-06-07 19:24:09 +0200

    first is the first cached object of the collection

--- lib/acts_as_cacheable.rb
+++ lib/acts_as_cacheable.rb
@@ -84,7 +84,7 @@ module Acts
   
       # Overwritten method, which returns the first object of the cached collection
       def first
-        all.detect{|x| !x.nil?}
+        all.first
       end
 
       # Overwritten method, which returns the last object of the cached collection
